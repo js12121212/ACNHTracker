d024069fde8c9a3e22df6cb3afd1bb3d25974938
Jon Staines <jon_staines@hotmail.com>
2020-07-28 11:30:59 +0100

    Changed data into 3 seperate files

---
+++ src/data/bugs.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    name: "Agrias Butterfly",
+    image: "agrias-butterfly.png",
+    northMonths: [4, 5, 6, 7, 8, 9],
+    southMonths: [11, 12, 1, 2, 3],
+    activeHours: [8, 9, 10, 11, 12, 13, 14, 15, 16],
+    location: "Flying",
+    price: 3000,
+  },
+];
---
+++ src/data/undersea.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    name: "Abalone",
+    image: "abalone.png",
+    northMonths: [6, 7, 8, 9, 10, 11, 12, 1],
+    southMonths: [12, 1, 2, 3, 4, 5, 6, 7],
+    activeHours: [16, 17, 18, 19, 20, 21, 22, 23, 0, 1, 2, 3, 4, 5, 6, 7, 8],
+    location: "Underwater",
+    price: 2000,
+  },
+];
