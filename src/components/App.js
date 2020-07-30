0251b797693331a5f61b602cf45a55f1c9deb14c
Jon Staines <jon_staines@hotmail.com>
2020-07-30 11:03:05 +0100

    Changed to card grid layout

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,16 @@ import MuseumItems from "./MuseumItems";
 const App = () => {
   return (
     <div>
-      <MuseumItems />
+      <div className="ui fixed inverted menu">
+        <div className="ui container">
+          <i className="calendar icon">Fish</i>
+          <i className="certificate icon">Bugs</i>
+          <i className="compass icon">Undersea</i>
+        </div>
+      </div>
+      <div className="ui main container">
+        <MuseumItems />
+      </div>
     </div>
   );
 };
--- src/components/MuseumItems.js
+++ src/components/MuseumItems.js
@@ -7,11 +7,19 @@ class MuseumItems extends React.Component {
   renderList() {
     return fish.map((item) => {
       return (
-        <div className="item" key={item.image}>
-          <img className="ui small image" src={`/images/fish/${item.image}`} />
+        <div className="card" key={item.image}>
+          <div className="image">
+            <img className="" src={`/images/fish/${item.image}`} />
+          </div>
           <div className="content">
             <div className="header">{item.name}</div>
-            {item.location}
+            <div className="description">
+              Month view
+              <br />
+              Hours
+              <br />
+              {item.location}
+            </div>
           </div>
         </div>
       );
@@ -20,8 +28,8 @@ class MuseumItems extends React.Component {
 
   render() {
     return (
-      <div>
-        <div className="ui celled list">{this.renderList()}</div>
+      <div className="ui container">
+        <div className="ui cards">{this.renderList()}</div>
       </div>
     );
   }
