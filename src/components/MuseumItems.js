6cd010301dd9f1049a91350f1fdfae580ac6a24f
Jon Staines <jon_staines@hotmail.com>
2020-07-30 11:49:45 +0100

    Added month hard coded to north for now and altered styles

--- src/components/MuseumItems.js
+++ src/components/MuseumItems.js
@@ -1,24 +1,49 @@
 import React from "react";
 import fish from "../data/fish.js";
+import months from "../data/months.js";
 // import LazyLoad from "react-lazyload";
 // import Loading from "./Loading";
 
 class MuseumItems extends React.Component {
+  renderMonths(itemActiveMonths) {
+    return months.map((month) => {
+      return (
+        <div
+          className={`column ${
+            itemActiveMonths.indexOf(month.id) > -1 ? "green" : ""
+          }`}
+        >
+          {month.name}
+        </div>
+      );
+    });
+  }
+
+  renderMonthContainer(itemActiveMonths) {
+    return (
+      <div className="ui six column grid">
+        {this.renderMonths(itemActiveMonths)}
+      </div>
+    );
+  }
+
   renderList() {
     return fish.map((item) => {
       return (
-        <div className="card" key={item.image}>
+        <div className="card" key={item.id}>
           <div className="image">
-            <img className="" src={`/images/fish/${item.image}`} />
+            <img alt={item.name} src={`/images/fish/${item.image}`} />
           </div>
           <div className="content">
             <div className="header">{item.name}</div>
             <div className="description">
-              Month view
-              <br />
-              Hours
-              <br />
-              {item.location}
+              <div className="ui segment">
+                {this.renderMonthContainer(item.northMonths)}
+              </div>
+
+              <div className="ui small header">Active Hours:</div>
+
+              <div className="ui small header">Found in : {item.location}</div>
             </div>
           </div>
         </div>
