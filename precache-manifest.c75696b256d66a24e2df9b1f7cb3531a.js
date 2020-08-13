7af9e4bc08b0328b6d99e702e4a5aca09072139b
Jon Staines <jon_staines@hotmail.com>
2020-08-13 15:21:18 +0100

    Updates

---
+++ precache-manifest.c75696b256d66a24e2df9b1f7cb3531a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7a4a090194fe46d28d443a05fbb2bb",
+    "url": "/ACNHTracker/index.html"
+  },
+  {
+    "revision": "c8ee651e973bc5169a5a",
+    "url": "/ACNHTracker/static/js/2.0edd2f95.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/ACNHTracker/static/js/2.0edd2f95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1444eca1f42e57da263",
+    "url": "/ACNHTracker/static/js/main.9ad6e20a.chunk.js"
+  },
+  {
+    "revision": "0ea8e613d030f447d477",
+    "url": "/ACNHTracker/static/js/runtime-main.aa61a72b.js"
+  }
+]);
